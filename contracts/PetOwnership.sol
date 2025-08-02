526b6b62e1bf17199958fb3d10b5a3af6d71c1a6
MitchWilkins <mitch.wilkins@me.com>
2025-08-02 09:00:57 -0500

    feat(NFT): create PetOwnershipNFT

--- contracts/PetOwnership.sol
+++ contracts/PetOwnership.sol
@@ -1,16 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PetOwnership is ERC721, Ownable {
+contract PetOwnership is ERC721URIStorage, Ownable {
     constructor(address initialOwner)
         ERC721("PetOwnership", "PET")
         Ownable(initialOwner)
     {}
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
-        _safeMint(to, tokenId);
+    event PetOwnershipMinted(address indexed to, uint256 indexed tokenId);
+
+    function safeMint(address _to, uint256 _tokenId, string calldata _ipfsHash) public onlyOwner {
+        _safeMint(_to, _tokenId);
+        _setTokenURI(_tokenId, string.concat("ipfs://", _ipfsHash));
+        emit PetOwnershipMinted(_to, _tokenId);
     }
 }
