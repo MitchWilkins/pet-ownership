89388e42fafeeacce172b4ea84e201ad3e6fd9bc
MitchWilkins <mitch.wilkins@me.com>
2025-08-01 19:27:52 -0500

    initial commit

--- contracts/PetOwnership.sol
+++ contracts/PetOwnership.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC721, Ownable {
+contract PetOwnership is ERC721, Ownable {
     constructor(address initialOwner)
-        ERC721("MyToken", "MTK")
+        ERC721("PetOwnership", "PET")
         Ownable(initialOwner)
     {}
 
